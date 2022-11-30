c333f2f9de4251b8ec560f8ba951b6200c062a91
lkpttxg <79006804+lkpttxg@users.noreply.github.com>
2022-11-30 09:49:05 +0000

    2022-11-11 Updated By Github Actions

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,1 @@
-window.addEventListener("scroll", percent)
-// 页面百分比
-function percent() {
-    // console.log("函数执行")
-    if(document.querySelector("#post")){
-        let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
-        result = Math.round(a / b * 100), // 计算百分比
-
-        up = document.querySelector("#post-percent"); // 获取按钮
-    up.childNodes[0].childNodes[0].innerHTML = result + "%";
-    }
-    
-}
+function percent(){if(document.querySelector("#post")){let e=document.documentElement.scrollTop||window.pageYOffset,t=Math.max(document.body.scrollHeight,document.documentElement.scrollHeight,document.body.offsetHeight,document.documentElement.offsetHeight,document.body.clientHeight,document.documentElement.clientHeight)-document.documentElement.clientHeight,n=Math.round(e/t*100);document.querySelector("#post-percent").childNodes[0].childNodes[0].innerHTML=n+"%"}}window.addEventListener("scroll",percent);
--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '( •̥́ ˍ •̀ू ) 主人, 不要走！再康康嘛！';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = 'ヾ(≧∪≦*)ノ〃 欢迎肥来！' + OriginTitile;
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="( •̥́ ˍ •̀ू ) 主人, 不要走！再康康嘛！",clearTimeout(titleTime)):(document.title="ヾ(≧∪≦*)ノ〃 欢迎肥来！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
